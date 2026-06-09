ba0205df8f204c3b0365f52ad0fb21718d87f3fa
AidanCox122 <aidancox122@gmail.com>
2026-06-09 08:13:12 -0700

    add lat/month selection to main bout_freq_analysis script

--- scripts/analysis/fine-scale-dive-analysis/bout_frequency_analysis.R
+++ scripts/analysis/fine-scale-dive-analysis/bout_frequency_analysis.R
@@ -228,3 +228,194 @@ emmeans::emmeans(freq.mod, ~cluster, type = 'response')
 emmeans::emmeans(freq.mod, ~species, type = 'response')
 
 
+# re-do the selection with month / lat ------------------------------------
+
+# full model
+f.mod1 <- 
+  bout_frequency %>% 
+  mutate(month = factor(month)) %>% 
+  glmmTMB(
+    bout_freq ~ cluster + species + month + lat + cluster:species + cluster:month + cluster:lat + species:month + species:lat + month:lat,
+    contrasts = list(cluster = 'contr.sum', species = 'contr.sum', month = 'contr.sum'),
+    family = beta_family(link = 'logit'),
+    REML = F,
+    data = .)
+
+f.mod2 <- 
+  bout_frequency %>% 
+  mutate(month = factor(month)) %>% 
+  glmmTMB(
+    bout_freq ~ cluster + species + month + lat + cluster:species + cluster:month + cluster:lat + species:month + species:lat + month:lat + (1|species:ptt),
+    contrasts = list(cluster = 'contr.sum', species = 'contr.sum', month = 'contr.sum'),
+    family = beta_family(link = 'logit'),
+    control = glmmTMBControl(optCtrl = list(iter.max = 1000, eval.max = 1000)),
+    REML = F,
+    data = .)
+
+f.mod3 <- 
+  bout_frequency %>% 
+  mutate(month = factor(month)) %>% 
+  glmmTMB(
+    bout_freq ~ cluster + species + month + lat + cluster:species + cluster:month + cluster:lat + species:month + species:lat + month:lat + (1|ptt),
+    contrasts = list(cluster = 'contr.sum', species = 'contr.sum', month = 'contr.sum'),
+    family = beta_family(link = 'logit'),
+    REML = F,
+    data = .)
+
+# get AIC weights
+vAIC <- AIC(f.mod1, f.mod2, f.mod3)[,2]
+dAIC <- vAIC - min(vAIC, na.rm = T)
+AICw <- exp(-dAIC/2) / sum(exp(-dAIC/2), na.rm = T)
+AICw #f.mod4 is hands down best
+
+drop1(f.mod2)
+
+summary(f.mod2)
+
+sim1 <- DHARMa::simulateResiduals(f.mod2)
+plot(sim1)
+
+plotQQunif(sim1)
+testDispersion(sim1) # no overdispersion
+
+## step 2
+# remove non-signif. interactions
+f.mod7 <- 
+  bout_frequency %>% 
+  mutate(month = factor(month)) %>% 
+  glmmTMB(
+    bout_freq ~ cluster + species + month + lat + cluster:lat + species:lat + month:lat + (1|species:ptt),
+    contrasts = list(cluster = 'contr.sum', species = 'contr.sum', month = 'contr.sum'),
+    family = beta_family(link = 'logit'),
+    control = glmmTMBControl(optCtrl = list(iter.max = 1000, eval.max = 1000)),
+    REML = F,
+    data = .)
+
+# test AIC weights
+vAIC <- AIC(f.mod2, f.mod7)[,2]
+dAIC <- vAIC - min(vAIC, na.rm = T)
+AICw <- exp(-dAIC/2) / sum(exp(-dAIC/2), na.rm = T)
+AICw
+
+
+sim2 <- DHARMa::simulateResiduals(f.mod7)
+plot(sim2)
+plotQQunif(sim2)
+testQuantiles(sim2)
+
+testCategorical(sim2, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(cluster)) # heteroscedastic
+testCategorical(sim2, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(species)) # 
+testCategorical(sim2, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(month)) # heteroscedastic
+testQuantiles(sim2, predictor = bout_frequency %>% filter(!is.na(lat)) %>% pull(lat))
+
+## step 3
+# assess a dispersion parameter
+f.mod8 <- 
+  bout_frequency %>% 
+  mutate(month = factor(month)) %>% 
+  glmmTMB(
+    bout_freq ~ cluster + species + month + lat + cluster:lat + species:lat + month:lat + (1|species:ptt),
+    contrasts = list(cluster = 'contr.sum', species = 'contr.sum', month = 'contr.sum'),
+    dispformula = ~ species,
+    family = beta_family(link = 'logit'),
+    control = glmmTMBControl(optCtrl = list(iter.max = 1000, eval.max = 1000)),
+    REML = F,
+    data = .)
+
+# test AIC weights
+vAIC <- AIC(f.mod7, f.mod8)[,2]
+dAIC <- vAIC - min(vAIC, na.rm = T)
+AICw <- exp(-dAIC/2) / sum(exp(-dAIC/2), na.rm = T)
+AICw
+
+sim3 <- DHARMa::simulateResiduals(f.mod8)
+plot(sim3)
+plotQQunif(sim3)
+testQuantiles(sim3)
+
+testCategorical(sim2, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(cluster)) # heteroscedastic
+testCategorical(sim2, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(species)) # 
+testCategorical(sim2, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(month)) # heteroscedastic
+testQuantiles(sim2, predictor = bout_frequency %>% filter(!is.na(lat)) %>% pull(lat))
+
+
+# evaluation --------------------------------------------------------------
+freq.mod <- 
+  bout_frequency %>% 
+  mutate(month = factor(month)) %>% 
+  glmmTMB(
+    bout_freq ~ cluster + species + month + lat + cluster:lat + species:lat + month:lat + (1|species:ptt),
+    contrasts = list(cluster = 'contr.sum', species = 'contr.sum', month = 'contr.sum'),
+    dispformula = ~ species,
+    family = beta_family(link = 'logit'),
+    control = glmmTMBControl(optCtrl = list(iter.max = 1000, eval.max = 1000)),
+    REML = T,
+    data = .)
+
+# DHARMa diagnostics
+sim <- DHARMa::simulateResiduals(freq.mod)
+plot(sim)
+plotQQunif(sim)
+testQuantiles(sim)
+
+testCategorical(sim, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(cluster)) # heteroscedastic
+testCategorical(sim, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(species)) # 
+testCategorical(sim, catPred = bout_frequency %>% filter(!is.na(lat)) %>% pull(month)) # heteroscedastic
+testQuantiles(sim, predictor = bout_frequency %>% filter(!is.na(lat)) %>% pull(lat))
+
+
+# check VIF
+performance::check_collinearity(freq.mod, component = 'all')
+
+# omnibus anova results
+car::Anova(freq.mod, type = 'III')
+
+# emmeans
+emmeans::emmeans(freq.mod, ~ cluster, type = 'response')
+emmeans::emmeans(freq.mod, ~ species, type = 'response')
+emmeans::emmeans(freq.mod, ~ month, type = 'response')
+
+# visualize interactions
+predict.data <-
+  bout_frequency %>% 
+  mutate(month = factor(month)) %>% 
+  filter(!is.na(lat)) %>% 
+  cbind(
+    predict(freq.mod, newdata = ., type = 'response', se = T))
+
+# cluster interaction
+predict.data %>% 
+  ggplot() +
+  geom_point(aes(x = lat, y = bout_freq, color = cluster), alpha = 0.15) +
+  geom_smooth(aes(x = lat, y = fit, color = cluster), se = F) +
+  scale_color_manual(values = c("yellow",
+                                "yellow3",
+                                "#488E9EFF",
+                                "#404C8BFF",
+                                "#281A2CFF")) +
+  theme_classic()
+
+# species interaction
+predict.data %>% 
+  ggplot() +
+  geom_point(aes(x = lat, y = bout_freq, color = species), alpha = 0.15) +
+  geom_smooth(aes(x = lat, y = fit, color = species), se = F) +
+  theme_classic()
+
+# month interaction
+tibble(
+  cluster = c('EPI 1', 'EPI 2', 'DVM 1', 'DVM 2', 'DVM 3') %>% rep(each = 8) %>% rep(times = 2) %>% rep(times = 4),
+  species = c('I.oxyrinchus', 'P.glauca') %>% rep(each = 40) %>% rep(times = 4),
+  month = c(9,10,11,12,1,2,3,4) %>% rep(times = 10) %>% rep(times = 4) %>% factor(),
+  lat = c(10.61, 31.60, 37.46, 43.53) %>% rep(each = 80)) %>% 
+  cbind(
+    predict(freq.mod, newdata = ., type = 'response', se = T, re.form = NA)) %>% 
+  mutate(lat = factor(lat)) %>% 
+  ggplot() +
+  geom_point(aes(x = month, y = fit, color = lat), alpha = 0.15) +
+  geom_smooth(aes(x = month, y = fit, color = lat, group = lat), se = F) +
+  theme_classic()
+
+
+
+
